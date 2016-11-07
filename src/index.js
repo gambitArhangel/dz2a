00e5e40ff9cd40596d4cbe7d7769f6cb5882904a
gambit <arhangel.2012.96@gmail.com>
2016-11-07 23:10:32 +0500

    Сделал дз, протестил всё работает

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express'
+
+const app = express();
+app.get('/', function (req, res) {
+  console.log(req.query);
+  console.log(parseInt(req.query.a) + parseInt(req.query.b));
+  const c = (parseInt(req.query.a) + parseInt(req.query.b));
+  // res.json({
+  //   a: req.query.a,
+  //   b: req.query.b,
+  //   c,
+  // })
+  res.send('a + b = ' + c)
+
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
